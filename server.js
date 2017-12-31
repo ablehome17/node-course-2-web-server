cfe70a70fe5c231f765e4c1e5a6635f3da698c47
Steve <ttmai@telstra.com>
2017-12-31 13:23:28 +1000

    Remove maintenance

--- server.js
+++ server.js
@@ -24,9 +24,9 @@ app.use(function (req,res,next){
     next();
 });
 
-app.use( function (req,res,next) {
-    res.render('maintenance.hbs');
-});
+// app.use( function (req,res,next) {
+//     res.render('maintenance.hbs');
+// });
 
 app.use(express.static(__dirname + '/public'));
 
